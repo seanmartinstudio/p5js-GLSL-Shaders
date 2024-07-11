bfcef9b8bb8f05406321ce10749081611ed12486
Sean Martin <sean@seanmartinstudio.com>
2024-07-11 10:26:58 -0700

    Uniforms exercise

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
-//Left off practice at "Basic Examples - Uniforms (Interactivity)"
-
-// a shader variable
+//this variable will hold our shader object
 let theShader;
 
 function preload(){
-  // load the shader
-  theShader = loadShader('shader.vert', 'shader.frag');
+  theShader = loadShader('uniforms.vert', 'uniforms.frag');
 }
 
 function setup() {
-  // disables scaling for retina screens which can create inconsistent scaling between displays
-  pixelDensity(1);
   // shaders require WEBGL mode to work
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(710, 400, WEBGL);
   noStroke();
 }
 
-function draw() {
-  // send resolution of sketch into shader
-  theShader.setUniform('u_resolution', [width, height]);
+function draw() {  
   // shader() sets the active shader with our shader
   shader(theShader);
+  
+  // lets send the resolution, mouse, and time to our shader
+  // before sending mouse + time we modify the data so it's more easily usable by the shader
+  theShader.setUniform('resolution', [width, height]);
+  theShader.setUniform('mouse', map(mouseX, 0, width, 0, 7));
+  theShader.setUniform('time', frameCount * 0.01);
+  
+  
   // rect gives us some geometry on the screen
-  rect(0,0,width,height);
-  // print out the framerate
-  //  print(frameRate());
+  rect(0,0,width, height);
+  console.log("theShader", theShader)
 }
 
 function windowResized(){
